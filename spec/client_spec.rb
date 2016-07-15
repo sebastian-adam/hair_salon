d78665893e67646f8522bcfb24811bf362c97f6f
Sebastian Adam Prokuski <Subconscious@Sebastians-iMac.local>
2016-07-15 16:33:02 -0700

    add passing method for 'return the phone number' for Client and Stylist

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -2,42 +2,42 @@ require('spec_helper')
 
 describe(Client) do
   describe('#first_name') do
-    it('returns the first name of a given client') do
+    it('returns the first name of a client') do
       client1 = Client.new({:id => nil, :first_name => 'Sue', :last_name => 'Davis', :phone_num => '7778889999', :typical_package => 'shampoo and trim', :stylist_id => 1})
       expect(client1.first_name()).to(eq('Sue'))
     end
   end
 
   describe('#last_name') do
-    it('returns the last name of a given client') do
+    it('returns the last name of a client') do
       client1 = Client.new({:id => nil, :first_name => 'Sue', :last_name => 'Davis', :phone_num => '7778889999', :typical_package => 'shampoo and trim', :stylist_id => 1})
       expect(client1.last_name()).to(eq('Davis'))
     end
   end
+
+  describe('#phon_num') do
+    it('returns the phone number of a client') do
+      client1 = Client.new({:id => nil, :first_name => 'Sue', :last_name => 'Davis', :phone_num => '7778889999', :typical_package => 'shampoo and trim', :stylist_id => 1})
+      expect(client1.phone_num()).to(eq('7778889999'))
+    end
+  end
   #
   # describe('#first_name') do
-  #   it('returns the first name of a given client') do
+  #   it('returns the first name of a client') do
   #     client1 = Client.new({:id => nil, :first_name => 'Sue', :last_name => 'Davis', :phone_num => '7778889999', :typical_package => 'shampoo and trim', :stylist_id => 1})
   #     expect(client1.first_name()).to(eq('Sue'))
   #   end
   # end
   #
   # describe('#first_name') do
-  #   it('returns the first name of a given client') do
+  #   it('returns the first name of a client') do
   #     client1 = Client.new({:id => nil, :first_name => 'Sue', :last_name => 'Davis', :phone_num => '7778889999', :typical_package => 'shampoo and trim', :stylist_id => 1})
   #     expect(client1.first_name()).to(eq('Sue'))
   #   end
   # end
   #
   # describe('#first_name') do
-  #   it('returns the first name of a given client') do
-  #     client1 = Client.new({:id => nil, :first_name => 'Sue', :last_name => 'Davis', :phone_num => '7778889999', :typical_package => 'shampoo and trim', :stylist_id => 1})
-  #     expect(client1.first_name()).to(eq('Sue'))
-  #   end
-  # end
-  #
-  # describe('#first_name') do
-  #   it('returns the first name of a given client') do
+  #   it('returns the first name of a client') do
   #     client1 = Client.new({:id => nil, :first_name => 'Sue', :last_name => 'Davis', :phone_num => '7778889999', :typical_package => 'shampoo and trim', :stylist_id => 1})
   #     expect(client1.first_name()).to(eq('Sue'))
   #   end
--- spec/stylist_spec.rb
+++ spec/stylist_spec.rb
@@ -2,28 +2,28 @@ require('spec_helper')
 
 describe(Stylist) do
   describe('#first_name') do
-    it('returns the first name of a given client') do
+    it('returns the first name of a stylist') do
       stylist1 = Stylist.new({:id => nil, :first_name => 'Betty', :last_name => 'Rogers', :phone_num => '1112223333'})
       expect(stylist1.first_name()).to(eq('Betty'))
     end
   end
 
   describe('#last_name') do
-    it('returns the last name of a given client') do
+    it('returns the last name of a stylist') do
       stylist1 = Stylist.new({:id => nil, :first_name => 'Betty', :last_name => 'Rogers', :phone_num => '1112223333'})
       expect(stylist1.last_name()).to(eq('Rogers'))
     end
   end
 
-  # describe('#first_name') do
-  #   it('returns the first name of a given client') do
-  #     stylist1 = Stylist.new({:id => nil, :first_name => 'Betty', :last_name => 'Rogers', :phone_num => '1112223333'})
-  #     expect(stylist1.first_name()).to(eq('Betty'))
-  #   end
-  # end
+  describe('#phone_num') do
+    it('returns the phone number of a stylist') do
+      stylist1 = Stylist.new({:id => nil, :first_name => 'Betty', :last_name => 'Rogers', :phone_num => '1112223333'})
+      expect(stylist1.phone_num()).to(eq('1112223333'))
+    end
+  end
   #
   # describe('#first_name') do
-  #   it('returns the first name of a given client') do
+  #   it('returns the first name of a stylist') do
   #     stylist1 = Stylist.new({:id => nil, :first_name => 'Betty', :last_name => 'Rogers', :phone_num => '1112223333'})
   #     expect(stylist1.first_name()).to(eq('Betty'))
   #   end
